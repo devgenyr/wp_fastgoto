fc7deaa300507bdd2dc27d1ec70807b276cb8096
evgner <evgner@gmail.com>
2017-09-10 15:15:28 +0600

    basic styles

--- inc/fgt-core.php
+++ inc/fgt-core.php
@@ -16,7 +16,24 @@ if ( ! class_exists( 'WPM_FastGoTo' ) ) {
 		function init() {
 			if ( is_admin_bar_showing() ) {
 				error_log('wpm_fgt_init');
+				add_action( 'admin_bar_menu', [&$this, 'setup_toolbar' ], 999 );
+				add_action( 'admin_enqueue_scripts', [&$this, 'add_assets' ] );
 			}
+		}
+
+		function add_assets() {
+			error_log('wpm_fgt_init2');
+			wp_register_style( 'wpm_fgt_style', plugins_url( '../assets/css/fgt-main.css', __FILE__ ), array(), null, 'all' );
+			wp_enqueue_style( 'wpm_fgt_style' );
+		}
+
+		function setup_toolbar( $wp_admin_bar ) {
+			$args = array(
+				'id'    => 'wpm_fgt_toolbar',
+				'title' => '<input type="text" class="wpm_fgt_search_input" placeholder="GoTo">',
+				// 'meta'  => array( 'class' => 'wpm_fgt_toolbar_class' )
+			);
+			$wp_admin_bar->add_node( $args );
 		}
 
 	}
