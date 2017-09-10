3be9df2dc94c0777aeb726cd69b63f26c826c7ce
evgner <evgner@gmail.com>
2017-09-10 13:02:06 +0600

    Initial commit

---
+++ fast-goto.php
@@ -0,0 +1,10 @@
+<?php
+/**
+Plugin Name: Fast Goto
+Plugin URI: http://wordpress.org/plugins/
+Description: This plugin adds a search field to admin bar which allows to quickly find and go to WordPress admin pages
+Author: WordPress Monsters
+Version: 1.0
+Author URI: https://www.wpmonsters.org/
+*/
+
