15ecaae5ed7976cf943db1887ddf0fe6a40fe17e
evgner <evgner@gmail.com>
2018-03-03 17:08:22 +0600

    save and load menu list functionality

--- inc/gt-core.php
+++ inc/gt-core.php
@@ -9,7 +9,7 @@ if ( ! class_exists( 'WPM_FastGoTo' ) ) {
 
 	class WPM_FastGoTo {
 
-		private $searchable = array();
+		private $searchable = [];
 
 		function __construct() {
 			add_action( 'init', [ &$this, 'init' ] );
@@ -17,9 +17,9 @@ if ( ! class_exists( 'WPM_FastGoTo' ) ) {
 
 		function init() {
 			if ( is_admin_bar_showing() ) {
+				$this->load_locations();
 				add_action( 'admin_enqueue_scripts', [ &$this, 'add_assets' ] );
 				add_action( 'wp_enqueue_scripts', [ &$this, 'add_assets' ] );
-				add_action( 'admin_init', [ &$this, 'get_locations' ] );
 				add_action( 'admin_bar_menu', [ &$this, 'setup_toolbar' ], 999 );
 			}
 		}
@@ -32,50 +32,52 @@ if ( ! class_exists( 'WPM_FastGoTo' ) ) {
 			wp_register_script( 'wpm_gt_main',  WPM_GT_PLUGIN_DIR_URL . 'assets/js/main.js', array( 'jquery', 'wpm_gt_fuzzy' ), null, true );
 			wp_enqueue_script( 'wpm_gt_main' );
 			wp_add_inline_script( 'wpm_gt_main', 'var wpm_gt_admin_url = "' . admin_url() . '";' ."\n" .
-			                                     'var wpm_gt_locations = ' . json_encode($this->searchable) . ';', 'before' );
+			                                     'var wpm_gt_locations = ' . json_encode( $this->searchable ) . ';', 'before' );
 		}
 
-		function get_locations() {
-			global $submenu, $menu, $pagenow;
+		function load_locations() {
 			if ( is_admin() ) {
-				$index = 0;
-				foreach ( $menu as $menu_item ) {
-					if ( isset( $menu_item[0] ) && ! empty( $menu_item[0] ) ) {
-						$main_menu_link = isset( $menu_item[2] ) && ! empty( $menu_item[2] ) ? $menu_item[2] : '';
-						$this->searchable[] = array(
-							'isSubmenu' => false,
-							'title' => wp_strip_all_tags( $menu_item[0], true ),
-							'link' => $main_menu_link,
-						);
-						$offset = 0;
-						foreach ( $submenu as $parent_link => $sub_items ) {
-							if ( $parent_link === $main_menu_link ) {
-								foreach ( $sub_items as $sub_item ) {
-									if ( isset( $sub_item[0] ) && ! empty( $sub_item[0] ) ) {
-										$offset += 1;
-										$sub_menu_link = isset( $sub_item[2] ) && ! empty( $sub_item[2] ) ? $sub_item[2] : '';
-										$this->searchable[] = array(
-											'isSubmenu' => true,
-											'title' => wp_strip_all_tags( $sub_item[0], true ),
-											'link' => $sub_menu_link,
-											'parent' => $index,
-										);
-									}
+				// update & store admin menu locations
+				add_action ( 'admin_init', [ &$this, 'update_locations' ] );
+			} else {
+				// if we are not in admin area then get locations from options
+				$this->searchable = get_option( 'wpm_gt_admin_menu_list', [] );
+			}
+		}
+
+		function update_locations() {
+			global $submenu, $menu, $pagenow;
+			$index = 0;
+			foreach ( $menu as $menu_item ) {
+				if ( isset( $menu_item[0] ) && ! empty( $menu_item[0] ) ) {
+					$main_menu_link = isset( $menu_item[2] ) && ! empty( $menu_item[2] ) ? $menu_item[2] : '';
+					$this->searchable[] = array(
+						'isSubmenu' => false,
+						'title' => wp_strip_all_tags( $menu_item[0], true ),
+						'link' => $main_menu_link,
+					);
+					$offset = 0;
+					foreach ( $submenu as $parent_link => $sub_items ) {
+						if ( $parent_link === $main_menu_link ) {
+							foreach ( $sub_items as $sub_item ) {
+								if ( isset( $sub_item[0] ) && ! empty( $sub_item[0] ) ) {
+									$offset += 1;
+									$sub_menu_link = isset( $sub_item[2] ) && ! empty( $sub_item[2] ) ? $sub_item[2] : '';
+									$this->searchable[] = array(
+										'isSubmenu' => true,
+										'title' => wp_strip_all_tags( $sub_item[0], true ),
+										'link' => $sub_menu_link,
+										'parent' => $index,
+									);
 								}
 							}
 						}
-						$index += $offset + 1;
 					}
+					$index += $offset + 1;
 				}
-				// error_log('wpm2');
-				// error_log(print_r($this->searchable, true));
-				// error_log(print_r(json_encode($this->searchable), true));
-				// error_log(print_r($pagenow, true));
-				// error_log(print_r($menu, true));
-				// error_log(print_r($submenu,true));
-			} else {
-				// get locations from cache
 			}
+			//store with disabled autoload
+			update_option( 'wpm_gt_admin_menu_list', $this->searchable, false );
 		}
 
 		function setup_toolbar( $wp_admin_bar ) {
